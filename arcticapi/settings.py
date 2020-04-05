294aa4f5eaeafc097532bd43daf7f5ec715b625c
Kyle Daines <kylerdaines@gmail.com>
2020-04-05 12:45:16 -0600

    tried again

--- arcticapi/settings.py
+++ arcticapi/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
